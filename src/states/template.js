bcef90f59a7084ddb5b5b6a23199728ae71571f2
serghov <sergey.hovakimyan@gmail.com>
2017-03-28 17:26:40 +0400

    added a template state

---
+++ src/states/template.js
@@ -0,0 +1,58 @@
+/**
+ * When making states copy this and modify!
+ */
+import State from '../GameMechanics/State.js';
+
+export const state_slide_1 = {
+    taron: new State('state_slide_1'),
+    cardboard: new State('state_slide_1'),
+    laptop: new State('state_slide_1'),
+};
+
+/**
+ * TARON
+ */
+
+state_slide_1.taron.on('start', (evt) => {
+    console.log('first state started');
+});
+
+state_slide_1.taron.on('finish', (evt) => {
+    console.log('first state finished');
+});
+
+state_slide_1.taron.on('fastForward', (evt) => {
+    console.log('first state fast-forwarded');
+});
+
+/**
+ * CARDBOARD
+ */
+
+state_slide_1.cardboard.on('start', (evt) => {
+    console.log('first state started');
+});
+
+state_slide_1.cardboard.on('finish', (evt) => {
+    console.log('first state finished');
+});
+
+state_slide_1.cardboard.on('fastForward', (evt) => {
+    console.log('first state fast-forwarded');
+});
+
+/**
+ * LAPTOP
+ */
+
+state_slide_1.laptop.on('start', (evt) => {
+    console.log('first state started');
+});
+
+state_slide_1.laptop.on('finish', (evt) => {
+    console.log('first state finished');
+});
+
+state_slide_1.laptop.on('fastForward', (evt) => {
+    console.log('first state fast-forwarded');
+});
