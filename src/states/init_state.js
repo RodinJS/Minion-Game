661839de8c5a8d46164f29b58d6183f24accdfba
serghov <sergey.hovakimyan@gmail.com>
2017-03-28 17:46:30 +0400

    state_init

---
+++ src/states/init_state.js
@@ -0,0 +1,55 @@
+import State from '../GameMechanics/State.js';
+
+export const state_init = {
+    taron: new State('state_init'),
+    cardboard: new State('state_init'),
+    laptop: new State('state_init'),
+};
+
+/**
+ * TARON
+ */
+
+state_init.taron.on('start', (evt) => {
+
+});
+
+state_init.taron.on('finish', (evt) => {
+
+});
+
+state_init.taron.on('fastForward', (evt) => {
+
+});
+
+/**
+ * CARDBOARD
+ */
+
+state_init.cardboard.on('start', (evt) => {
+
+});
+
+state_init.cardboard.on('finish', (evt) => {
+
+});
+
+state_init.cardboard.on('fastForward', (evt) => {
+
+});
+
+/**
+ * LAPTOP
+ */
+
+state_init.laptop.on('start', (evt) => {
+
+});
+
+state_init.laptop.on('finish', (evt) => {
+
+});
+
+state_init.laptop.on('fastForward', (evt) => {
+
+});
