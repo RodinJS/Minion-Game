4dc14c5e7cad71d2aa772cfeb29c624ffe5a8bae
chriskarap <karapetyanchristina@gmail.com>
2017-04-03 16:07:43 +0400

    RO-623 # integrated circle grabbing into current codebase

--- src/states/state_ball_flying.js
+++ src/states/state_ball_flying.js
@@ -1,56 +1,58 @@
 import State from '../GameMechanics/State.js';
+import * as R from 'rodin/core';
 
 const startBallFlying = (evt) => {
     const ball = evt.globals.ball;
+    R.Scene.add(ball);
     const ballFinalPosition = new R.utils.Vector3(0, 5, 5);
 
     const lerpBall = () => {
         if(ball.position.distanceTo(ballFinalPosition) < .1) {
-            // todo: go to next state
             ball.removeEventListener(R.CONST.UPDATE, lerpBall);
+            evt.gameMechanics.next();
             return;
         }
         ball.position.lerp(ballFinalPosition, .1);
     };
 
-    ball.addEventListener(R.CONST.UPDATE,);
+    ball.addEventListener(R.CONST.UPDATE, lerpBall);
 };
 
-export const state_init = {
-    taron: new State('state_init'),
-    cardboard: new State('state_init'),
-    laptop: new State('state_init'),
+export const state_ball_flying = {
+    taron: new State('state_ball_flying'),
+    cardboard: new State('state_ball_flying'),
+    laptop: new State('state_ball_flying'),
 };
 
 /**
  * TARON
  */
 
-state_init.taron.on('start', (evt) => {
-    startBallFlying();
+state_ball_flying.taron.on('start', (evt) => {
+    startBallFlying(evt);
 });
 
-state_init.taron.on('finish', (evt) => {
+state_ball_flying.taron.on('finish', (evt) => {
 
 });
 
-state_init.taron.on('fastForward', (evt) => {
-    startBallFlying();
+state_ball_flying.taron.on('fastForward', (evt) => {
+    startBallFlying(evt);
 });
 
 /**
  * CARDBOARD
  */
 
-state_init.cardboard.on('start', (evt) => {
+state_ball_flying.cardboard.on('start', (evt) => {
 
 });
 
-state_init.cardboard.on('finish', (evt) => {
+state_ball_flying.cardboard.on('finish', (evt) => {
 
 });
 
-state_init.cardboard.on('fastForward', (evt) => {
+state_ball_flying.cardboard.on('fastForward', (evt) => {
 
 });
 
@@ -58,14 +60,14 @@ state_init.cardboard.on('fastForward', (evt) => {
  * LAPTOP
  */
 
-state_init.laptop.on('start', (evt) => {
+state_ball_flying.laptop.on('start', (evt) => {
 
 });
 
-state_init.laptop.on('finish', (evt) => {
+state_ball_flying.laptop.on('finish', (evt) => {
 
 });
 
-state_init.laptop.on('fastForward', (evt) => {
+state_ball_flying.laptop.on('fastForward', (evt) => {
 
 });
